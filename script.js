5a5866b2c20a1f62eb9c8fe672092c70fc7322e5
Vineet Singh <78719701+Vineet829@users.noreply.github.com>
2024-09-19 02:07:54 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,13 @@
 function makeid(l) {
-  // write your code here
+     let arr = [String.fromCharCode(Math.floor(Math.random() * (57 - 48 + 1) + 48)),
+		String.fromCharCode(Math.floor(Math.random() * (90 - 65 + 1) + 65)),
+		String.fromCharCode(Math.floor(Math.random() * (122 - 97 + 1) + 97))]
+	let result = []
+	
+	for(let i = 0; i < l; i++){
+            result.push(arr[Math.floor(Math.random() *  (2-0 + 1) + 0)])      		
+	}
+	 return result.join("")
 }
 
 // Do not change the code below.
